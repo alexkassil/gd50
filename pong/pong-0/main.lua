ad32dcd4f2b631a245528de81b718a1e6c925c32
Alex Kassil <alexkassil@sbcglobal.net>
2018-05-17 02:31:49 -0700

    Add pong-0 code with comments

--- pong/pong-0/main.lua
+++ pong/pong-0/main.lua
@@ -1,2 +1,43 @@
-WINDOW_HEIGHT = 1080
-WINDOW_WIDTH = 720
+--[[
+	GD50 2018
+	Pong Remake
+
+	pong-0
+	"The Day-0 Update"
+]]
+
+--[[
+	Global Variables to set window resolution
+	16:9 Aspect Ratio
+]]
+
+WINDOW_HEIGHT = 540
+WINDOW_WIDTH = 960
+
+--[[
+	love.load always runs on startup
+	Set window resolution
+	Passing in a table with different values
+	Not fullscreen, not resizable, synced to moniter refresh rate
+]]
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+	    fullscreen = false,
+		resizable = false,
+		vsync = true
+	})
+end
+
+--[[
+	love.draw is used to draw anything to screen
+]]
+
+function love.draw()
+    love.graphics.printf(
+		'Hello Pong!',
+		0,
+		WINDOW_HEIGHT / 2 - 6,
+		WINDOW_WIDTH,
+		'center')
+end
