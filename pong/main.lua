a4078bb01d5b36af0f7e9e4c2f60de93b8f9b94c
Alex Kassil <alexkassil@sbcglobal.net>
2018-05-19 02:16:57 -0700

    Add paddle movement

--- pong/main.lua
+++ pong/main.lua
@@ -26,6 +26,9 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+-- speed at which we move the paddle
+PADDLE_SPEED = 200
+
 --[[
 	love.load always runs on startup
 	Set window resolution
@@ -39,6 +42,9 @@ function love.load()
 
 	-- more retro font object
 	smallFont = love.graphics.newFont('font.ttf', 8)
+
+	scoreFont = love.graphics.newFont('font.ttf', 32)
+
 	love.graphics.setFont(smallFont)
 	
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -46,6 +52,39 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+	player1Score = 0
+	player2Score = 0
+
+	player1Y = 30
+	player2Y = VIRTUAL_HEIGHT - 50
+end
+
+--[[
+	Runs every fram, with "dt" being our change in seconds
+	since last frame, given by LOVE2D
+	We scale our speed by delta time to have consistend movement
+	regardless of FPS
+--]]
+
+function love.update(dt)
+	-- player 1 movement
+	if love.keyboard.isDown('w') then
+		-- add negative paddle speed to current Y scaled by deltaTime
+		player1Y = player1Y - PADDLE_SPEED * dt
+	elseif love.keyboard.isDown('s') then
+		-- add positive paddle speed to current Y scaled by deltaTime
+		player1Y = player1Y + PADDLE_SPEED * dt
+	end
+
+	-- player 2 movement
+	if love.keyboard.isDown('up') then
+		-- add negative paddle speed to current Y scaled by deltaTime
+		player2Y = player2Y - PADDLE_SPEED * dt
+	elseif love.keyboard.isDown('down') then
+		-- add positive paddle speed to current Y scaled by deltaTime
+		player2Y = player2Y + PADDLE_SPEED * dt
+	end
 end
 
 --[[
@@ -72,10 +111,10 @@ function love.draw()
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
 	-- render left paddle
-	love.graphics.rectangle('fill', 10, 30, 5, 20)
+	love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
 	-- render right paddle
-	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
 	-- render ball
 	love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
